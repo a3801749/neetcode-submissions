7a43ada4a248439e653380373180e26d7e067dd3
a3801749 <110496889+a3801749@users.noreply.github.com>
2026-05-28 23:30:55 -0400

    Add: contains-duplicate-ii - submission-0

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        for (int i = 0; i < nums.size()-1; i++) {
+            for (int j = 1; j <= k; j++) {
+                if (i + j >= nums.size()) { continue; }
+                if (nums[i] == nums[i + j]) {
+                    return true;
+                }
+            }
+        }
+        return false; 
+    }
+};
