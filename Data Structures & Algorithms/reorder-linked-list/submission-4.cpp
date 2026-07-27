2363518168af3d049ce6bdc8570a22ecbe78e868
a3801749 <110496889+a3801749@users.noreply.github.com>
2026-07-27 01:32:50 -0400

    Add: reorder-linked-list - submission-4

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-4.cpp
@@ -0,0 +1,59 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        if (head == nullptr || head -> next == nullptr) { return; }
+        ListNode* prev_slow = nullptr;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast->next != nullptr && fast->next->next != nullptr) {
+            fast = fast->next->next;
+            prev_slow = slow;
+            slow = slow->next;
+        }
+        ListNode* second = reverseList(slow->next);
+        slow->next = nullptr;
+        ListNode* after_head = head->next;
+        ListNode* after_second = second->next;
+        while (second != nullptr) {
+            after_head = head->next;
+            after_second = second->next;
+
+            head->next = second;
+            second->next = after_head;
+
+            // if (after_head == nullptr && after_second != nullptr) {
+            //     second->next = after_second;
+            // }
+
+            head = after_head;
+            second = after_second;
+        }
+    }
+
+    ListNode* reverseList(ListNode* head) {
+        if (head == nullptr) { return head; }
+
+        ListNode* current = head->next;
+        head->next = nullptr;
+        ListNode* after_current;
+
+        while (current != nullptr) {
+            after_current = current->next;
+            current->next = head;
+            head = current;
+            current = after_current;
+        }
+        return head;
+    }
+};
